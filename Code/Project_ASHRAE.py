9ee91aacba24004fe99aa26d6275a69039a1ffd2
Zsolt Nagy <Zsolt.Nagy1@ibm.com>
2019-12-03 15:12:43 +0100

    Adding_Data
    Adding Data files and import packages cell to Notebook

--- Code/Project_ASHRAE.py
+++ Code/Project_ASHRAE.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# <h1 align="center"> <span style="color:darkslategray">ASHRAE</span><h1><h2 span align="center"><span style="color:darkslategray">A Nagy energia előrejelző versenypályázat III</h2><br/><h3 span align="center"><span style="color:darkslategray">Mennyi energiát fogyaszt egy épület?</span></h3>
+
+# ### <span style="color:dimgray"> Csomagok importálása</span>
+
 # In[ ]:
 
 
+# Csomagok importálása validációhoz
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn import metrics, model_selection
 
+# Csomagok importálása vizualizációhoz
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Csomagok importálása modellezéshez
+import xgboost as xgb
+import catboost as cbt
+import lightgbm as lgbm
+
+# Általános csomagok importálása
+import pandas as pd
+import numpy as nd
 
