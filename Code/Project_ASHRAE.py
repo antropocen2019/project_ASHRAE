818f6fa06cf4258fd9c47fc5c278b8e5dd2bc6d9
Zsolt Nagy <Zsolt.Nagy1@ibm.com>
2019-12-09 16:08:11 +0100

    Update_Notebook
    Adding memory usage reduction and data-set merging parts to the notebook

--- Code/Project_ASHRAE.py
+++ Code/Project_ASHRAE.py
@@ -59,7 +59,7 @@
 
 # ### <span style="color:dimgray"> Csomagok importálása</span>
 
-# In[14]:
+# In[1]:
 
 
 # Csomagok importálása validációhoz
@@ -80,20 +80,21 @@ import lightgbm as lgbm
 
 # Általános csomagok importálása
 import pandas as pd
-import numpy as nd
+import numpy as np
 import os
+from IPython.display import display_html 
 
 
 # ### <span style="color:dimgray"> Adatok betöltése </span>
 
-# In[34]:
+# In[ ]:
 
 
 # Az elérési út megadása
 root = 'C:/Users/ZsoltNagy/Desktop/github_projects/ASHRAE/project_ASHRAE/Data'
 
 
-# In[22]:
+# In[3]:
 
 
 # Adatok betöltése
@@ -104,7 +105,7 @@ df_weather_test = pd.read_csv(os.path.join(root, 'weather_test.csv'))
 df_building = pd.read_csv(os.path.join(root, 'building_metadata.csv'))
 
 
-# In[32]:
+# In[4]:
 
 
 # Gyors pillantás az adatokra
@@ -115,7 +116,7 @@ display(df_weather_test.iloc[:10,:])
 display(df_building.iloc[:10,:])
 
 
-# In[38]:
+# In[5]:
 
 
 # Alapinformációk lekérése az adatszetekről
@@ -133,6 +134,121 @@ df_building.info()
 # ### <span style="color:dimgray"> Memóriahasználat csökkentése </span>
 
 # #### A train és test adatszetek memóriahasználata jelentős, mely hatékonyan csökkenthető a változók konvertálásával
+
+# In[6]:
+
+
+## Függvény a memóriahasználat csökkentéséhez. Forrás: https://www.kaggle.com/caesarlupum/ashrae-start-here-a-gentle-introduction
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+
+# In[7]:
+
+
+# A memóriahasználatot csökkentő függvény alkalmazása
+
+df_train_red = reduce_mem_usage(df_train)
+df_test_red = reduce_mem_usage(df_test)
+df_weather_train_red = reduce_mem_usage(df_weather_train)
+df_weather_test_red = reduce_mem_usage(df_weather_test)
+df_building_red = reduce_mem_usage(df_building)
+
+
+# In[9]:
+
+
+# Alapstatisztikák lekérése az adatok transzformáció előtti és utáni összehasonlításához:
+
+# df_train
+desc_dftrain = df_train.describe()
+desc_dftrain_red = df_train_red.describe()
+
+desc_dftrain_styler = desc_dftrain.style.set_table_attributes("style='display:inline'").set_caption('Eredeti df_train')
+desc_dftrain_red_styler = desc_dftrain_red.style.set_table_attributes("style='display:inline'").set_caption('Transzformált df_train')
+
+display_html(desc_dftrain_styler._repr_html_()+desc_dftrain_red_styler._repr_html_(), raw=True)
+
+# df_test
+desc_dftest = df_test.describe()
+desc_dftest_red = df_test_red.describe()
+
+desc_dftest_styler = desc_dftest.style.set_table_attributes("style='display:inline'").set_caption('Eredeti df_test')
+desc_dftest_red_styler = desc_dftest_red.style.set_table_attributes("style='display:inline'").set_caption('Transzformált df_test')
+
+display_html(desc_dftest_styler._repr_html_()+desc_dftest_red_styler._repr_html_(), raw=True)
+
+# df_weather_train
+desc_dfweather_train = df_weather_train.describe()
+desc_dfweather_train_red = df_weather_train_red.describe()
+
+desc_dfweather_train_styler = desc_dfweather_train.style.set_table_attributes("style='display:inline'").set_caption('Eredeti df_weather_train')
+desc_dfweather_train_red_styler = desc_dfweather_train_red.style.set_table_attributes("style='display:inline'").set_caption('Transzformált df_weather_train')
+
+display_html(desc_dfweather_train_styler._repr_html_()+desc_dfweather_train_red_styler._repr_html_(), raw=True)
+
+# df_weather_test
+desc_dfweather_test = df_weather_test.describe()
+desc_dfweather_test_red = df_weather_test_red.describe()
+
+desc_dfweather_test_styler = desc_dfweather_test.style.set_table_attributes("style='display:inline'").set_caption('Eredeti df_weather_test')
+desc_dfweather_test_red_styler = desc_dfweather_test_red.style.set_table_attributes("style='display:inline'").set_caption('Transzformált df_weather_test')
+
+display_html(desc_dfweather_test_styler._repr_html_()+desc_dfweather_test_red_styler._repr_html_(), raw=True)
+
+# building
+desc_dfbuilding = df_building.describe()
+desc_dfbuilding_red = df_building_red.describe()
+
+desc_dfbuilding_styler = desc_dfbuilding.style.set_table_attributes("style='display:inline'").set_caption('Eredeti df_building')
+desc_dfbuilding_red_styler = desc_dfbuilding_red.style.set_table_attributes("style='display:inline'").set_caption('Transzformált df_building')
+
+display_html(desc_dfbuilding_styler._repr_html_()+desc_dfbuilding_red_styler._repr_html_(), raw=True)
+
+
+# ### <span style="color:dimgray"> Adatszetek egyesítése </span>
+
+# In[14]:
+
+
+train = df_train_red.merge(df_building_red, on='building_id', how='left')
+test = df_test_red.merge(df_building_red, on='building_id', how='left')
+
+train = train.merge(df_weather_train_red, on=['site_id', 'timestamp'], how='left')
+test = test.merge(df_weather_test_red, on=['site_id', 'timestamp'], how='left')
+
+display(train.iloc[:10,:])
+display(test.iloc[:10,:])
+
+
+# ### <span style="color:dimgray"> Adatok feltérképezése </span>
+
+# #### Az adatok feltérképezése egy iteratív, végnélküli folyamat, mely során igyekszünk megérteni az adatban rejlő mintázatokat, összefüggéseket, trendeket, valamint anomáliákat alapvető statistikai eljárások használata révén. 
 
 # In[ ]:
 
